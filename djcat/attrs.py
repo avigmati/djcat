9d9b2567b461a75fccd3fcabe933ad06de590e3b
avigmati <avigmati@gmail.com>
2017-01-21 00:56:27 +0700

    attributes and items collect with decorators

--- djcat/attrs.py
+++ djcat/attrs.py
@@ -1,18 +1,97 @@
+from django.conf import settings
+
+from djcat.exceptions import ItemAttributeUnknownType, ItemAttributeKeyNotPresent, ItemAttributeKeyDuplicate
+
+
 def catalog_attribute(name=None, key=None, verbose_name=None):
     def decorate(cls):
-        setattr(cls, 'DJCAT_ATTRIBUTE_CLASS', cls)
-        setattr(cls, 'DJCAT_ATTRIBUTE_KEY', key)
-        setattr(cls, 'DJCAT_ATTRIBUTE_NAME', name)
-        setattr(cls, 'DJCAT_ATTRIBUTE_VERBOSE_NAME', verbose_name if verbose_name else name)
+        setattr(cls, '_attr_class', cls)
+        if not key:
+            raise ItemAttributeKeyNotPresent(cls)
+        setattr(cls, 'attr_key', key)  # must be unique
+        setattr(cls, 'attr_name', name)
+        setattr(cls, 'attr_verbose_name', verbose_name if verbose_name else name)
         return cls
     return decorate
 
 
+def slug_attribute(cls):
+    cls._slug_methods = {}
+    cls._is_slug = True
+    for methodname in dir(cls):
+        method = getattr(cls, methodname)
+        if hasattr(method, '_slug'):
+            try:
+                getattr(cls, method._slug_validator_func)
+            except AttributeError as e:
+                raise Exception('slug validator func not exist')
+            cls._slug_methods.update({methodname: {'validator_func': method._slug_validator_func}})
+    return cls
+
+
+def slug(*args, **kwargs):
+    try:
+        validator_func = kwargs.pop('validator')
+    except KeyError:
+        raise Exception('slug validator not specified')
+
+    def wrapper(func):
+        func._slug = True
+        func._slug_validator_func = validator_func
+        return func
+    return wrapper
+
+
 class BaseAttribute:
-    DJCAT_ATTRIBUTE_CLASS = None
-    DJCAT_ATTRIBUTE_KEY = None
-    DJCAT_ATTRIBUTE_NAME = None
-    DJCAT_ATTRIBUTE_VERBOSE_NAME = None
+    """
+    Base item attribute class.
+     Subclasses must define attributes:
+     DJCAT_ATTRIBUTE_TYPE - unique key, don't repeat anywhere!
+     DJCAT_ATTRIBUTE_KEY - unique key, don't repeat anywhere!
+     DJCAT_ATTRIBUTE_NAME - attribute name
+     DJCAT_ATTRIBUTE_VERBOSE_NAME = attribute verbose name, it shows user
+
+     These attributes can be specified through catalog_attribute decorator, example:
+
+     from djcat.attrs import BaseAttribute, catalog_attribute
+
+        @catalog_attribute(name='Area', verbose_name='Area', key='ar')
+        class AreaAttribute(BaseAttribute):
+            pass
+    """
+    attr_key = None
+    attr_name = None
+    attr_verbose_name = None
+    attr_type = None
+    _attr_class = None
+
+    def __init__(self, *args, **kwargs):
+        if self.__class__.attr_type not in settings.DJCAT_ATTR_TYPES:
+            raise ItemAttributeUnknownType(self.__class__._attr_class, self.__class__.attr_type)
+
+    @classmethod
+    def get_type(cls):
+        """
+        Return attribute type
+        :return: String
+        """
+        return cls.attr_type
+
+    @classmethod
+    def get_class(cls):
+        """
+        Return attribute class path
+        :return: String
+        """
+        return '{}.{}'.format(cls._attr_class.__module__, cls._attr_class.__name__)
+
+    @classmethod
+    def get_key(cls):
+        """
+        Return attribute key
+        :return: String
+        """
+        return cls.attr_key
 
     @classmethod
     def get_name(cls):
@@ -20,4 +99,120 @@ class BaseAttribute:
         Return attribute name
         :return: String
         """
-        return cls.DJCAT_ATTRIBUTE_NAME
+        return cls.attr_name
+
+    @classmethod
+    def get_verbose_name(cls):
+        """
+        Return attribute verbose name
+        :return: String
+        """
+        return cls.attr_verbose_name
+
+    def get_values_for_registry(self, registry):
+        self.validate(registry)
+        return {
+            'type': self.get_type(),
+            'verbose_name': self.get_verbose_name(),
+            'key': self.get_key(),
+            'class': self.get_class(),
+            '_class': self
+        }
+
+    def validate(self, registry):
+        self.check_attr_key(registry)
+        if getattr(self, '_is_slug', False):
+            self.validate_specials(registry)
+
+    def check_attr_key(self, registry):
+        """
+        Check attribute key for duplicates in registry
+        :return:
+        """
+        for m in registry.items():
+            for i in m[1]['items'].items():
+                for a in i[1]['attrs'].items():
+                    if a[1]['key'] == self.attr_key:
+                        raise ItemAttributeKeyDuplicate(a[1]['class'], self._attr_class, self.attr_key)
+
+    def validate_specials(self, registry):
+        """
+        Validate special methods of attribute class
+        :param registry: Dictionary CatalogItem.REGISTRY
+        :return:
+        """
+        for _, spec_d in self.__class__._specials.items():
+            validator = getattr(self.__class__, spec_d['validator_func'])
+            validator(self, registry)
+
+
+class SimplyAttribute(BaseAttribute):
+    attr_type = 'simply'
+
+    # def __init__(self, *args, **kwargs):
+    #     print(self.__class__.attr_type)
+
+
+class ItemAttributeChoicesSlugsDuplicate(Exception):
+    def __init__(self, attr_class):
+        self.attr_class = attr_class
+        self.error = "Attribute class '{}' duplicate choices slugs"
+
+    def __repr__(self):
+        return self.error.format(self.attr_class)
+
+    def __str__(self):
+        return self.error.format(self.attr_class)
+
+
+@slug_attribute
+class ChoiceAttribute(BaseAttribute):
+    attr_type = 'choice'
+    attr_choices = None
+
+    @classmethod
+    def get_choices_for_model_field(cls):
+        return [c[0:2] for c in cls.attr_choices]
+
+    def get_choices_slugs(self):
+        return [c[-1] for c in self.__class__.attr_choices]
+
+    @slug(validator='choices_slugs_validator')
+    def get_choices_slugs_for_registry(self):
+        return {'choices': self.get_choices_slugs()}
+
+    def choices_slugs_validator(self, registry):
+        self.check_choices_slugs()
+        self.check_catalog_item_choices_slugs(registry)
+        # check for duplicates in catalog item all choices
+
+        # print()
+
+    def check_choices_slugs(self):
+        """
+        Check for slug duplicates in attribute class choices
+        :return:
+        """
+        slugs = self.get_choices_slugs()
+        if not len(set(slugs)) == len(slugs):
+            raise ItemAttributeChoicesSlugsDuplicate(self)
+
+    def check_catalog_item_choices_slugs(self, registry):
+        """
+        Check for slug duplicates in catalog item class all choices
+        :param registry: Dictionary - CatalogItem.REGISTRY
+        :return:
+        """
+        slugs = self.get_choices_slugs()
+
+        for m in registry.items():
+            for i in m[1]['items'].items():
+                # for a in [a for a in i[1]['attrs'].items() ]:
+                for a in [a for a in i[1]['attrs'].items() if a[1]['type'] == 'choice']:
+                    print()
+                    if a[1]['key'] == self.attr_key:
+                        raise ItemAttributeKeyDuplicate(a[1]['class'], self._attr_class, self.attr_key)
+
+
+    def check_categories(self):
+        pass
--- djcat/models.py
+++ djcat/models.py
@@ -10,8 +10,8 @@ from django.utils.translation import ugettext as _
 from mptt.models import MPTTModel, TreeForeignKey
 
 from .utils import create_slug, unique_slug
-from .exceptions import CategoryInheritanceError, ItemModuleNameNotDefined, ItemModuleNameDuplicate, ItemNameDuplicate, \
-    CategoryRootCheckError, ItemAttributeKeyNotPresent, ItemAttributeKeyDuplicate
+from .exceptions import CategoryInheritanceError, ItemModuleNameNotDefined, ItemModuleNameDuplicate, \
+    ItemNameDuplicate, CategoryRootCheckError
 
 
 class BaseDjcat:
@@ -251,7 +251,8 @@ class CatalogItem:
         """
         module_name, module = self.register_module(cls)
         self.__class__.REGISTRY[module_name] = self.register_item(module, cls)
-        self.check_attr_values()
+        self.get_items_attributes()
+        print()
 
     def register_item(self, module, cls):
         """
@@ -271,33 +272,33 @@ class CatalogItem:
         :param cls: Class object
         :return: name: String - module name, module: Dictionary, module data
         """
-        name, verbose_name = self.get_module_name(cls)
+        name, verbose_name, _module = self.get_module(cls)
         if self.__class__.REGISTRY.get(name):
             module = self.__class__.REGISTRY[name]
         else:
-            module = {'module': cls.__module__, 'verbose_name': verbose_name, 'items': {}}
+            module = {'module': cls.__module__, 'verbose_name': verbose_name, 'items': {}, '_module': _module}
         return name, module
 
-    def get_module_name(self, cls):
+    def get_module(self, cls):
         """
         Return given class item module name (ITEM_MODULE_NAME)
         :param cls: Class object
         :return: String
         """
         try:
-            m = importlib.import_module(cls.__module__)
-            name = m.ITEM_MODULE_NAME
-            verbose_name = getattr(m, 'ITEM_MODULE_VERBOSE_NAME', name)
+            module = importlib.import_module(cls.__module__)
+            name = module.ITEM_MODULE_NAME
+            verbose_name = getattr(module, 'ITEM_MODULE_VERBOSE_NAME', name)
         except AttributeError:
             mnames = cls.__module__.split('.')
             if len(mnames) > 1:
-                root_module = importlib.import_module(mnames[0])
+                module = importlib.import_module(mnames[0])
                 try:
-                    name = root_module.ITEM_MODULE_NAME
+                    name = module.ITEM_MODULE_NAME
                 except AttributeError:
-                    raise ItemModuleNameNotDefined(cls, root_module)
+                    raise ItemModuleNameNotDefined(cls, module)
                 else:
-                    verbose_name = getattr(root_module, 'ITEM_MODULE_VERBOSE_NAME', name)
+                    verbose_name = getattr(module, 'ITEM_MODULE_VERBOSE_NAME', name)
             else:
                 raise ItemModuleNameNotDefined(cls, mnames[0])
 
@@ -309,7 +310,7 @@ class CatalogItem:
                 if mprops['verbose_name'] == verbose_name:
                     raise ItemModuleNameDuplicate(cls.__module__, verbose_name, mprops['module'])
 
-        return name, verbose_name
+        return name, verbose_name, module
 
     def get_item_class_props(self, module, cls):
         """
@@ -320,36 +321,79 @@ class CatalogItem:
         # check duplicates
         if self.verbose_name in [x[1]['verbose_name'] for x in module['items'].items()]:
             raise ItemNameDuplicate(self.verbose_name, module['module'])
-        return {'verbose_name': self.verbose_name, 'class_name': cls.__name__, 'class': '{}.{}'.format(module['module'], cls.__name__),
-                'attributes': self.get_class_attributes(cls)}
+        return {'verbose_name': self.verbose_name, 'class_name': cls.__name__,
+                'class': '{}.{}'.format(module['module'], cls.__name__), '_class': cls, 'attrs': {}}
 
-    def get_class_attributes(self, cls):
-        """
-        Return given class attributes
-        :param cls: Class object
-        :return: Dictionary
-        """
-        attrs = {}
-        for f in cls._meta.fields:
-            if getattr(f, 'DJCAT_ATTRIBUTE_NAME', None):
-                attrs[f.DJCAT_ATTRIBUTE_NAME] = {'verbose_name': f.DJCAT_ATTRIBUTE_VERBOSE_NAME,
-                                                 'key': f.DJCAT_ATTRIBUTE_KEY,
-                                                 'class': '{}.{}'.format(f.DJCAT_ATTRIBUTE_CLASS.__module__,
-                                                                         f.DJCAT_ATTRIBUTE_CLASS.__name__)}
-        return attrs
-
-    def check_attr_values(self):
-        """
-        Validate items attributes values
-        :return:
-        """
-        akeys = []
+    def get_items_attributes(self):
         for m in self.__class__.REGISTRY.items():
             for i in m[1]['items'].items():
-                for a in i[1]['attributes'].items():
-                    if not a[1].get('key', None):
-                        raise ItemAttributeKeyNotPresent(a[1]['class'])
-                    if not a[1]['key'] in akeys:
-                        akeys.append(a[1]['key'])
-                    else:
-                        raise ItemAttributeKeyDuplicate(a[1]['class'], a[1]['key'])
+                for f in i[1]['_class']._meta.fields:
+                    if getattr(f, '_attr_class', None):
+                        a = f._attr_class()
+                        i[1]['attrs'].update({
+                            f.get_name(): a.get_values_for_registry(self.__class__.REGISTRY)
+                        })
+
+    def get_attr(self, attr_class):
+        attr = attr_class
+        attr.validate(self.__class__.REGISTRY)
+        return {
+            'type': attr.get_type(),
+            'verbose_name': attr.get_verbose_name(),
+            'key': attr.get_key(),
+            'class': attr.get_class(),
+            '_class': attr_class
+        }
+    #
+    #
+    #
+    #     # attrs[f.get_name()] = {
+    #     #     'type': a.get_type(),
+    #     #     'verbose_name': a.get_verbose_name(),
+    #     #     'key': a.get_key(),
+    #     #     'class': a.get_class(),
+    #     #     '_class': f._attr_class
+    #     # }
+    #
+    # def get_class_attributes(self, cls):
+    #     """
+    #     Return given class attributes
+    #     :param cls: Class object
+    #     :return: Dictionary
+    #     """
+    #     attrs = {}
+    #     for f in cls._meta.fields:
+    #         if getattr(f, '_attr_class', None):
+    #             a = f._attr_class()
+    #             a.validate(self.__class__.REGISTRY)
+    #             attrs[f.get_name()] = {
+    #                 'type': a.get_type(),
+    #                 'verbose_name': a.get_verbose_name(),
+    #                 'key': a.get_key(),
+    #                 'class': a.get_class(),
+    #                 '_class': f._attr_class
+    #             }
+    #     return attrs
+
+    # def check_attr_values(self):
+    #     """
+    #     Validate items attributes values
+    #     :return:
+    #     """
+    #     akeys = []
+    #     for m in self.__class__.REGISTRY.items():
+    #         for i in m[1]['items'].items():
+    #             for a in i[1]['attributes'].items():
+    #                 if not a[1].get('key', None):
+    #                     raise ItemAttributeKeyNotPresent(a[1]['class'])
+    #                 if not a[1]['key'] in akeys:
+    #                     akeys.append(a[1]['key'])
+    #                 else:
+    #                     raise ItemAttributeKeyDuplicate(a[1]['class'], a[1]['key'])
+
+
+class Path:
+    """
+    TODO: update REGISTRY with paths?
+    """
+    pass
